2330ed9180d55ad795da7e9fd511aed7d12571ea
alex <alex@epostnet.de>
2022-12-19 11:09:16 +0100

    Added persistent Storage

--- src/main/java/confusedalex/nonetherfasttravel/Events.java
+++ src/main/java/confusedalex/nonetherfasttravel/Events.java
@@ -2,17 +2,35 @@ package confusedalex.nonetherfasttravel;
 
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerPortalEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import java.util.HashMap;
 import java.util.UUID;
 
 public class Events implements Listener {
-
+    NoNetherFastTravel noNetherFastTravel;
     HashMap<UUID, Location> locationHashMap = new HashMap<>();
+
+    public Events(NoNetherFastTravel noNetherFastTravel) {
+        this.noNetherFastTravel = noNetherFastTravel;
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        UUID uuid = e.getPlayer().getUniqueId();
+        FileConfiguration config = noNetherFastTravel.getConfig();
+
+        if (config.contains(uuid.toString())) {
+            locationHashMap.put(uuid, (Location) config.get(uuid.toString()));
+            config.set(uuid.toString(), null);
+        }
+    }
 
     @EventHandler
     public void OnPortalEvent (PlayerPortalEvent event){
@@ -25,6 +43,17 @@ public class Events implements Listener {
         } else if (location.getWorld().getEnvironment().equals(World.Environment.NETHER) && locationHashMap.containsKey(uuid)){
             event.setCancelled(true);
             player.teleport(locationHashMap.get(uuid));
+            locationHashMap.remove(uuid);
+        }
+    }
+
+    @EventHandler
+    public void onLeave(PlayerQuitEvent e) {
+        UUID uuid = e.getPlayer().getUniqueId();
+        FileConfiguration config = noNetherFastTravel.getConfig();
+
+        if (locationHashMap.containsKey(uuid)) {
+            config.set(uuid.toString(), locationHashMap.get(uuid));
             locationHashMap.remove(uuid);
         }
     }
--- src/main/java/confusedalex/nonetherfasttravel/NoNetherFastTravel.java
+++ src/main/java/confusedalex/nonetherfasttravel/NoNetherFastTravel.java
@@ -1,14 +1,18 @@
 package confusedalex.nonetherfasttravel;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NoNetherFastTravel extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new Events(), this);
+        getServer().getPluginManager().registerEvents(new Events(this), this);
     }
 
     @Override
-    public void onDisable() {}
+    public void onDisable() {
+
+    }
+
 }
